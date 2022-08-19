655308583234af1aef34b363d2b9bfa8b246501b
civil-Ken <tochukwukeze@gmail.com>
2022-08-19 22:26:24 +0200

    comment

--- Python Projects/my_guessing_game.py
+++ Python Projects/my_guessing_game.py
@@ -1,3 +1,11 @@
+#The program randomly selects a number between 1 and 100 or any other combination of 
+    #numbers.
+#It will then ask the player to enter his proposal.
+#It will then check if this number is the same as the one generated randomly by the 
+    #computer; if so, the player wins.
+#If the player’s guess is not the same, then he will check if the number is higher or 
+    #lower than the guess and tell the player.
+
 import random
 n= random.randrange (3,9)
 guess= input("guess a number 3-9: ")
